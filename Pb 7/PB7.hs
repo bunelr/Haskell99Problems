90eb68a13e31068fe8d13eb7b812362777995b0e
Rudy Bunel <bunel.rudy@gmail.com>
2013-11-17 20:47:08 +0100

    Pb 7 solved

---
+++ Pb 7/PB7.hs
@@ -0,0 +1,14 @@
+module PB7(
+NestedList,
+flatten
+)
+where
+
+data NestedList a = Elem a | List [NestedList a]
+
+flatten:: NestedList a->[a]
+flatten (Elem a) = [a]
+flatten (List []) = []
+flatten (List x) = flatten (head x) ++ flatten (List (tail x))
+
+
